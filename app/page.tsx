d3009beb218d7ac1e97832dafbfa7c2be8aa3440
LD <ldproject365@gmail.com>
2026-09-16 02:39:22 +0700

    refactor: update home auth import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import { verifySession } from "@/lib/auth";
+import { verifySession } from "@/src/backend/auth";
 
 export default async function Home() {
   const session = await verifySession((await cookies()).get("market_briefing_session")?.value);
